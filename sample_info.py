68837c1da1a71349966b5854a9594322ad786251
Garvit Sharma <61928763+gtxPrime@users.noreply.github.com>
2022-08-31 09:41:08 +0530

    Add files via upload

---
+++ sample_info.py
@@ -0,0 +1,32 @@
+# Bot information
+SESSION = 'FindYourMovie'
+USER_SESSION = 'User_Bot'
+API_ID = 12345
+API_HASH = '0123456789abcdef0123456789abcdef'
+BOT_TOKEN = '123456:FindYourMovie-zyx57W2v1u123ew11'
+USERBOT_STRING_SESSION = 'FindYourMovie'
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
+
+# Admins, Channels & Users
+ADMINS = [12345789, 'admin123', 987654321]
+CHANNELS = [-10012345678, -100987654321, 'FindYourMoviebot']
+AUTH_USERS = []
+AUTH_CHANNEL = None
+
+# MongoDB information
+DATABASE_URI = "Mongodb url"
+DATABASE_NAME = 'FindYourMovie'
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+
+# Messages
+START_MSG = """
+**Hi, I'm Media Search bot**
+
+Here you can search files in inline mode. Just press follwing buttons and start searching.
+"""
+
+SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
+INVITE_MSG = 'Please join @.... to use this bot'
